34ea5fccd6c2fcbed93edf7606d0f3ecac302780
Shlok Verma <117856077+ShlokVerma9282@users.noreply.github.com>
2024-07-20 14:43:13 +0530

    Time: 9 ms (57.56%), Space: 18.6 MB (34.73%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& nums) {
-        int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
-        // First pass to find potential candidates
-        for (auto num : nums) {
-            if (candidate1 == num) count1++;
-            else if (candidate2 == num) count2++;
-            else if (count1 == 0) candidate1 = num, count1 = 1;
-            else if (count2 == 0) candidate2 = num, count2 = 1;
-            else count1--, count2--;
+    vector<int> majorityElement(vector<int>& v) {
+        int n = v.size(); 
+
+    int cnt1 = 0, cnt2 = 0; 
+    int el1 = INT_MIN; 
+    int el2 = INT_MIN; 
+
+    
+    for (int i = 0; i < n; i++) {
+        if (cnt1 == 0 && el2 != v[i]) {
+            cnt1 = 1;
+            el1 = v[i];
         }
-         // Second pass to verify candidates
-        count1 = count2 = 0;
-        for (int num : nums) {
-            if (num == candidate1) count1++;
-            else if (num == candidate2) count2++;
+        else if (cnt2 == 0 && el1 != v[i]) {
+            cnt2 = 1;
+            el2 = v[i];
         }
+        else if (v[i] == el1) cnt1++;
+        else if (v[i] == el2) cnt2++;
+        else {
+            cnt1--, cnt2--;
+        }
+    }
+
+    vector<int> ls; 
+
+
+    cnt1 = 0, cnt2 = 0;
+    for (int i = 0; i < n; i++) {
+        if (v[i] == el1) cnt1++;
+        if (v[i] == el2) cnt2++;
+    }
+
+    int mini = int(n / 3) + 1;
+    if (cnt1 >= mini) ls.push_back(el1);
+    if (cnt2 >= mini) ls.push_back(el2);
+
+
+    return ls;
         
-        vector<int> ans;
-        if (count1 > nums.size() / 3) ans.push_back(candidate1);
-        if (count2 > nums.size() / 3) ans.push_back(candidate2);
-        
-        return ans;
     }
 };
