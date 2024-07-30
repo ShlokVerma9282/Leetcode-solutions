87ed67c335c89d3a245ec1f4c4055e1e8ec9877e
Shlok Verma <117856077+ShlokVerma9282@users.noreply.github.com>
2024-07-30 17:23:23 +0530

    Time: 0 ms (100.00%), Space: 8 MB (16.63%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> memo(m, vector<int>(n, -1));
-        return dp(m-1, n-1, memo);
+    int countWaysUtil(int m, int n, vector<vector<int>>& dp) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == 0 && j == 0) {
+                dp[i][j] = 1;
+                continue; 
+            }
+            int up = 0;
+            int left = 0;
+            if (i > 0)
+                up = dp[i - 1][j];
+            if (j > 0)
+                left = dp[i][j - 1];
+            dp[i][j] = up + left;
+        }
     }
-
-private:
-    int dp(int m, int n, vector<vector<int>> &memo) {
-        if (m < 0 || n < 0) return 0;
-        if (m == 0 && n == 0) return 1;
-        if (memo[m][n] != -1) return memo[m][n];
-
-        memo[m][n] = dp(m-1, n, memo) + dp(m, n-1, memo);
-        return memo[m][n];
-    }
+    return dp[m - 1][n - 1];
+}
+int uniquePaths(int m, int n) {
+    vector<vector<int>> dp(m, vector<int>(n, -1));
+    return countWaysUtil(m, n, dp);
+}
 };
